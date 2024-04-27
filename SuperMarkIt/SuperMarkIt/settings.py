e6a12713d1d27a827bbfbbc2d31e5ebdf7aec3aa
PORIPIREDDI JYOTHIRMAI <147608966+PoripireddiJyothirmai@users.noreply.github.com>
2024-04-27 06:16:37 +0530

    Update settings.py

--- SuperMarkIt/SuperMarkIt/settings.py
+++ SuperMarkIt/SuperMarkIt/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'SuperMarkIt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  BASE_DIR/'Aprems',
+        'NAME':  BASE_DIR/'Aprem',
         'USER': 'postgres',
         'PASSWORD': '30942',
         'HOST': 'localhost',
